23f361a7ca7205f28552b39afdaf6d9a7027b81e
Renan <renan.ferreira.p@gmail.com>
2025-06-17 16:27:02 -0300

    Criado componente tabela-gastos

---
+++ src/app/tabela-gastos/tabela-gastos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TabelaGastosComponent } from './tabela-gastos.component';
+
+describe('TabelaGastosComponent', () => {
+  let component: TabelaGastosComponent;
+  let fixture: ComponentFixture<TabelaGastosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TabelaGastosComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TabelaGastosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tabela-gastos/tabela-gastos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-gastos',
+  imports: [],
+  templateUrl: './tabela-gastos.component.html',
+  styleUrl: './tabela-gastos.component.css'
+})
+export class TabelaGastosComponent {
+
+}
